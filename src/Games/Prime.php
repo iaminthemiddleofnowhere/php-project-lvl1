260401a68bba40eb1f42116a8a81c977307fd1e1
Defrel <defrelka1@gmail.com>
2022-02-15 00:05:49 +0300

    fix Prime

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -9,7 +9,7 @@ function game($name, $rounds = 3)
 {
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
     for ($i = 0; $i < $rounds; $i++) {
-        $num = rand(2, 100);
+        $num = rand(2, 4);
         $rightAnswer = isPrime($num) ? "yes" : "no";
 
         line("Question: %s", $num);
@@ -20,7 +20,7 @@ function game($name, $rounds = 3)
 function isPrime($num)
 {
     $limit = $num / 2;
-    for ($i = 2; $i < $limit; $i++) {
+    for ($i = 2; $i <= $limit; $i++) {
         if ($num % $i === 0) {
             return false;
         }
