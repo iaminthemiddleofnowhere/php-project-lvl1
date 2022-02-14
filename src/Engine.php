4b25e95c582b121dde6403a28926b4fed7c40130
Defrel <defrelka1@gmail.com>
2022-02-15 01:53:03 +0300

    fix shitty error from shitty package

--- src/Engine.php
+++ src/Engine.php
@@ -7,10 +7,17 @@ use function cli\line;
 
 function startGame(string $game)
 {
+    $mapping = [
+        'even' => fn($name) => \Brain\Games\Even\game($name),
+        'calc' => fn($name) => \Brain\Games\Calc\game($name),
+        'gcd' => fn($name) => \Brain\Games\Gcd\game($name),
+        'progression' => fn($name) => \Brain\Games\Progression\game($name),
+        'prime' => fn($name) => \Brain\Games\Prime\game($name),
+    ];
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    $game($name);
+    $mapping[$game]($name);
     line("Congratulations, %s!", $name);
 }
 
